98b574a4c9de2d944e648cd42ca445ac5fc1fda9
hojinida <opqrs1103@gmail.com>
2024-07-27 21:07:13 +0900

    서비스 계층 BookService.java Kotlin으로 변경하기

---
+++ src/main/kotlin/com/group/libraryapp/util/ExceptionUtils.kt
@@ -0,0 +1,8 @@
+package com.group.libraryapp.util
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.findByIdOrNull
+
+fun fail(): Nothing = throw IllegalArgumentException()
+
+fun <T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID): T = this.findByIdOrNull(id) ?: fail()
